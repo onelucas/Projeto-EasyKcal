78189070dca3a18ee1f3298f18d9c2d2736f2579
Lucas <lucasviniciusmartins2004@gmail.com>
2025-03-20 17:51:24 -0300

    Login/import DB

--- src/index.php
+++ src/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include('conexao.php');
 
 if (isset($_POST['email']) || isset($_POST['senha']))
@@ -12,7 +13,7 @@ if (isset($_POST['email']) || isset($_POST['senha']))
         $email = $mysqli->real_escape_string($_POST['email']);
         $senha = $mysqli->real_escape_string($_POST['senha']);
 
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha= '$senha'";
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha  = '$senha'";
         $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
 
         $quantidade = $sql_query->num_rows;
@@ -63,4 +64,4 @@ if (isset($_POST['email']) || isset($_POST['senha']))
     </form>
 </body>
 
-</html>
+</html> 
--- src/protect.php
+++ src/protect.php
@@ -1,10 +1,11 @@
 <?php
+session_start();
 
 if(!isset($_SESSION)) {
     session_start();
 }
 
-if(!isset($_SESSION['id'])) {
+if(!isset($_SESSION['user'])) {
     die("Você não pode acessar esta página porque não está logado.<p><a href=\"index.php\">Entrar</a></p>");
 }
 
